c2e8646b382821db64042e7a544936fb1de1bb49
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-10-02 22:34:21 -0400

    Antigravity gun kind of works

---
+++ Assets/Scripts/Objects/AntigravityGun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntigravityGun : MonoBehaviour
+{
+    public Rigidbody PlayerRigidBody;
+    public float ReloadTimer = 1.0f;
+
+    private float currentReload;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentReload = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentReload += Time.deltaTime;
+        
+        Debug.DrawRay(transform.parent.transform.position, transform.parent.transform.forward * 8.0f, Color.green);
+
+        // If a button is clicked 
+        if(Input.GetAxis("Fire1") > 0 && currentReload > ReloadTimer) {
+            currentReload = 0.0f;
+            // Raycast    
+                // Modify force based on that (use a curve my man)
+                // update rigibody with boom
+            //(float explosionForce, Vector3 explosionPosition, float explosionRadius, float upwardsModifier = 0.0f, ForceMode mode = ForceMode.Force)); 
+            PlayerRigidBody.AddExplosionForce(2000.0f, transform.parent.transform.position + transform.parent.transform.forward * 2.0f, 50.0f);
+            Debug.Log("Boom");
+        }
+    }
+}
--- TODO.md
+++ TODO.md
@@ -1,11 +1,14 @@
 # TODO List 
 
+## Current
+
+ * Add anti-gravity gun (think rocket launcher but without the big boom)
+    * Distance from a surface will determine how far you go 
+
+## Main ones
 
  * Add levels 
  * Add beeps and boops sound effects 
- * Add anti-gravity gun (think rocket launcher but without the big boom)
-    * Distance from a surface will determine how far you go 
-    * Surface material will also effect this
  * Switch to a physics based movement system 
 
 ## Completed but needs love

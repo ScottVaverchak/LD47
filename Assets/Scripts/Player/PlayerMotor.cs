f3c2b6999238199f49cefec340212dce2aa53b7b
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-10-02 20:05:55 -0400

    Walking around

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+
+    public float RunSpeed = 1.0f;
+
     public GameObject Camera;
     public GameObject Feet;
 
@@ -29,11 +32,23 @@ public class PlayerMotor : MonoBehaviour
         }
 
         var jump = Input.GetAxis("Jump");
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
 
         if(jump > 0 && isGrounded) { 
             rb.AddForce(Vector3.up * 100.0f);
             isGrounded = false;
         }
+
+        Debug.DrawRay(Camera.transform.position, Camera.transform.forward * 2.0f, Color.red);
+
+        var np = (new Vector3(v, 0, h)).normalized;
+
+        if(v != 0) 
+            transform.position +=  Camera.transform.forward * Time.deltaTime * np.x * RunSpeed;
+
+        if(h != 0)
+            transform.position +=  Camera.transform.right * Time.deltaTime * np.z * RunSpeed;
     }
 
     void FixedUpdate() 
